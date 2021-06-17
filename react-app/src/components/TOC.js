2e3bd31cbe97f5db6bea6f239353f71dfd60bc42
bae-sh <b5460881@naver.com>
2021-06-17 19:00:15 +0900

    react

---
+++ react-app/src/components/TOC.js
@@ -0,0 +1,25 @@
+import {Component } from 'react';
+
+class TOC extends Component {
+    render() {
+      var lists = [];
+      var data=this.props.data;
+      var i = 0;
+      while(i<data.length){
+        lists.push(<li key={data[i].id}> <a href={"/content/"} onClick={function(id, e){
+          e.preventDefault();
+          this.props.onChangePage(id);
+        }.bind(this, data[i].id)}>{data[i].title} </a></li> );
+        i=i+1;
+      }
+      return(
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+    </nav>
+      );
+    }
+  }
+
+  export default TOC;
